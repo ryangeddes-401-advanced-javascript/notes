7dfef49ac7c5d6aee97b401d7fc66dcc2fbe3ebc
RYAN GEDDES <rgeddes09@gmail.com>
2020-08-19 11:35:58 -0700

    class changes didn't seem to add on previous commit

---
+++ callback.js
@@ -0,0 +1,41 @@
+
+'use strict';
+
+const fs = require('fs');
+
+
+/* 
+  Write a function called isEven that receives 2 parameters:
+    A Number
+    A Callback function
+
+  Determine if the number is even. If so, invoke the callback with true as an argument, otherwise, false
+
+  -----
+
+  Create an array of numbers.
+
+  Iterate the array with a for loop, each time calling the isEven function with the number and a callback
+  
+  The callback should console.log() the return value from isEven()
+
+*/
+
+
+
+
+/* 
+  Write a function called contents that receives 1 parameter:
+    A File Name
+
+  Using the fs module, open and read the contents of the file,converting them to text and printing the content to the console.
+
+  Print a console.error() if anything goes wrong.
+
+  You can test this by calling your function with './words.txt', which is a file that exists, and then again with the name of a file that does not exist.
+*/
+
+
+
+
+
